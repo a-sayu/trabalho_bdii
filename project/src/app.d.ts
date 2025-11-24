b88771cb192eb2953df47b3adc91f1c49d91d5bd
Abigail Sayury <abigailsayury.nakashima@gmail.com>
2025-11-24 19:04:19 -0300

    feat: Enhance authentication handling with role-based access control and session management

--- project/src/app.d.ts
+++ project/src/app.d.ts
@@ -1,13 +1,29 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import type { DefautSession } from "@auth/core/types";
+
 declare global {
 	namespace App {
+        interface Locals {
+
+        }
+        interface PageData {
+            session: Session | null;
+        }
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
+}
+
+declare module "@auth/core/types" {
+    interface Session {
+        user: {
+            role?: string;
+        } & DefaultSession["user"];
+    }
 }
 
 export {};
--- project/src/hooks.server.ts
+++ project/src/hooks.server.ts
@@ -1,30 +1,98 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/core/providers/google";
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, AUTH_SECRET } from "$env/static/private";
+import { redirect, type Handle } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
 
-// NOTICE THE { handle } DESTRUCTURING BELOW
-export const { handle } = SvelteKitAuth({
-  providers: [
-    Google({ 
-      clientId: GOOGLE_CLIENT_ID, 
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
+
+const ALLOWED_EMAILS = [
+    "cacic.fct@gmail.com",
+];
+
+const ADMIN_EMAILS = [
+    "abigail.s.nakashima@unesp.br",
+    "miguel.moret@unesp.br",
+    "cristian.eidi@unesp.br",
+    "daniel.aa.padua@unesp.br",
+    "paulo.sc.lima@unesp.br",
+    "filipe.nava@unesp.br",
+    "arthur.koichi@unesp.br",
+];
+
+const ADMIN_ROUTES = [
+    "/autorizar-evento",
+    "/gerenciar-pessoas",
+    "/log-page",
+    "/registrar-presenca",
+]
+
+const PROTECTED_ROUTES = [
+    "/adicionar-eventos",
+    "/adicionar-provas",
+    "/emitir-certificado",
+    "/gerar-qrcode",
+]
+
+const { handle: authHandle } = SvelteKitAuth({
+    providers: [
+        Google({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
+        }),
+    ],
+    secret: AUTH_SECRET,
+    trustHost: true,
+    callbacks: {
+        async signIn({ profile }) {
+            const email = profile?.email || "";
+            if (email.endsWith("@unesp.br")|| ALLOWED_EMAILS.includes(email)) {
+                return true;
+            }
+            return false;
+        },
+        async jwt({ token, user }) {
+            if (user && user.email) {
+                token.role = ADMIN_EMAILS.includes(user.email) ? "admin" : user;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.role = token.role as string;
+            }
+            return session;
         }
-      }
-    }),
-  ],
-  secret: AUTH_SECRET,
-  trustHost: true,
-  callbacks: {
-    async signIn({ profile }) {
-      if (profile?.email?.endsWith("@unesp.br")) {
-        return true;
-      }
-      return false; 
     }
-  }
 });
+
+const authorizationHandle: Handle = async ({ event, resolve} ) => {
+    const session = await event.locals.auth();
+    const pathname = event.url.pathname;
+
+    const isRouteAdmin = ADMIN_ROUTES.some(route=> pathname.startsWith(route));
+    const isRouteProtected = PROTECTED_ROUTES.some(route => pathname.startsWith(route));
+
+    if (!session) {
+        throw redirect(303, "/login");
+    } else {
+        if (isRouteAdmin) {
+            if (session.user?.role !== "admin") {
+                throw redirect(303, "/");
+            }
+        }
+    
+        if (isRouteProtected) {
+        }
+    }
+
+    return resolve(event);
+}
+
+export const handle = sequence(authHandle, authorizationHandle);
